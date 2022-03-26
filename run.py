938045c8576f344ed4b078f314f0b347e57a9b40
lukeexer <lientuna@gmail.com>
2022-03-26 17:23:42 +0800

    catch all exception in main function.

--- run.py
+++ run.py
@@ -1,16 +1,28 @@
+# pylint: disable=W0703
 """Server entry point."""
+import traceback
+
 from flask import Flask
 
 from slib.init import SInit
+from slib.log import SLog
 
 from api.example.views import example
 
 if __name__ == "__main__":
-    SInit.init()
+    try:
+        SInit.init()
 
-    app = Flask(__name__)    
-    app.env="development"
+        app = Flask(__name__)
+        app.env="development"
 
-    app.register_blueprint(example)
+        app.register_blueprint(example)
 
-    app.run(debug=False)
+        app.run(debug=False)
+
+    except Exception as e:
+        SLog.error(e)
+        SLog.error(traceback.format_exc())
+
+        print(e)
+        print(traceback.format_exc())
